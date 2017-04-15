b405f2767cc3f09a843b49ebdbd972979cf112b2
VitalyNikonorov <nik.vitaly@mail.ru>
2017-04-15 22:25:43 +0300

    add tabbar for history screen

--- translator/src/main/java/nikonorov/net/translator/screens/historyscreen/HistoryActivity.java
+++ translator/src/main/java/nikonorov/net/translator/screens/historyscreen/HistoryActivity.java
@@ -2,8 +2,12 @@ package nikonorov.net.translator.screens.historyscreen;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import nikonorov.net.translator.R;
 import nikonorov.net.translator.screens.historyscreen.presenter.HistoryScreenPresenterImpl;
@@ -15,17 +19,56 @@ import nikonorov.net.translator.screens.listscreen.view.ListFragment;
  * email@nikonorov.net
  */
 
-public class HistoryActivity extends AppCompatActivity {
+public class HistoryActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
+
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.history_screen);
-        final ViewPager viewPager = (ViewPager) findViewById(R.id.history_view_pager);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        viewPager = (ViewPager) findViewById(R.id.history_view_pager);
         final ListFragment[] fragments = new ListFragment[2];
         fragments[0] = ListFragment.getInstance(new HistoryScreenPresenterImpl());
         fragments[1] = ListFragment.getInstance(new SavedTranslationsPresenterImpl());
         final ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), fragments);
         viewPager.setAdapter(adapter);
+        final TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.tab_caption_history)));
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.tab_caption_bookmarks)));
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
     }
 }
